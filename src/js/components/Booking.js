42c5a6139842c06ae9e8281c016cf776c9e273c2
Julia Melnik <yuliyamielnik@gmail.com>
2020-04-15 17:30:49 +0200

    fix booking mistake

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,5 @@
 
-import { select, templates,  } from '../settings.js';
+import { select, templates } from '../settings.js';
 import { utils } from '../utils.js';
 import { AmountWidget } from './AmountWidget.js';
 import { DatePicker } from './DatePicker.js';
@@ -19,15 +19,17 @@ export class Booking {
 
     const thisBooking = this;
     const generatedHTML = templates.bookingWidget();
-    thisBooking.dom = {}; 
+    thisBooking.dom = {};
     thisBooking.dom.wrapper = element;
-    thisBooking.dom.wrapper = utils.createDOMFromHTML(generatedHTML);
+    thisBooking.generatedDOM = utils.createDOMFromHTML(generatedHTML);
+    //console.log(generatedHTML);
+
    
     thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount); 
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
     thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
     thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
-    //thisBooking.dom.wrapper.appendChild(thisBooking.dom.wrapper);
+    thisBooking.dom.wrapper.appendChild(thisBooking.generatedDOM);
   }
 
 
